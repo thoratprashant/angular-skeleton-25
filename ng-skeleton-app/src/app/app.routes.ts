f5846cf0059176b5247eae7740ee4cc218295f97
thoratprashant <prashant.thorat@arkenea.com>
2025-07-22 12:20:43 +0530

    Added Auth Module

--- ng-skeleton-app/src/app/app.routes.ts
+++ ng-skeleton-app/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        loadComponent: () => import('./layouts/auth/auth.component').then(m => m.AuthComponent),
+        children: [
+            {
+                path: 'login',
+                loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent),
+                title: 'Login'
+            },
+            {
+                path: 'forgot-password',
+                loadComponent: () => import('./auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent),
+                title: 'forgot Password'
+            },
+            { path: '', redirectTo: 'login', pathMatch: 'full' }
+        ]
+    },
+    { path: '', redirectTo: 'auth/login', pathMatch: 'full' }
+];
